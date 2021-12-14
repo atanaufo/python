4566340abce57bead7b32fa365c4cd1edd851a1e
Atanaufo Ramalho <atanaufo.ramalho@gmail.com>
2021-12-13 23:11:06 -0300

    Atualizações

--- pythonbásico/aula2-entrada-e-saida.py
+++ pythonbásico/aula2-entrada-e-saida.py
@@ -3,18 +3,18 @@
 # print('tabulação em \t execução')
 
 # nome = 'Atanaufo'
-nome = input('Escreve seu nome: ')  # Para receber informação do usuário
+nome = input('Escreve seu nome:')  # Para receber informação do usuário
 idade = 40
 altura = 1.72
 tipo_idade = type(idade)
 tipo_altura = type(altura)
 
-print(nome, 'tem',idade, 'anos e possui a altura de', altura, 'cm')
-print(tipo_idade)
-print(altura)
-print(str(altura))
+print(nome, 'tem', idade, '\tanos e possui a altura de', altura, 'cm')
+print('Tipo de dados \ Idade: ' , tipo_idade)
+print('Tipo de dados \ Altura: ' ,altura)
+print('Tipo de dados \ Altura: ',str(altura))
 print(type(nome), type(idade), type(altura))
-
+print('============================================================================')
 numero1 = 16
 numero2 = 2
 numero3 = 5.5
