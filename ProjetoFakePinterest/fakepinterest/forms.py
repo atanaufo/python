270c8c5efde114ec5871f282626f1d5ba06fcd92
Atanaufo Ramalho <atanaufo.ramalho@gmail.com>
2023-05-15 18:13:19 -0300

    Atualizações do dia 15/05/2023. Até a Aula 11 - Formulário de Login.

--- ProjetoFakePinterest/fakepinterest/forms.py
+++ ProjetoFakePinterest/fakepinterest/forms.py
@@ -1,12 +1,26 @@
 # criar os formulários do nosso site.
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
+from fakepinterest.models import Usuario
+class FormLogin(FlaskForm):
+    email = StringField("E-mail", validators=[DataRequired(), Email()])
+    senha = PasswordField("Senha", validators=[DataRequired()])
+    botao_confirmacao = SubmitField("Fazer Login")
+
+class FormCriarConta(FlaskForm):
+    email = StringField("E-mail", validators=[DataRequired(), Email()])
+    username = StringField("Nome de Usuário", validators=[DataRequired()])
+    senha = PasswordField("Senha", validators=[DataRequired(), Length(6, 20)])
+    confirmacao_senha = PasswordField("Confirmação de Senha", validators=[DataRequired(), EqualTo("senha")])
+    botao_confirmacao = SubmitField("Criar Conta")
+
+    def validate_email(self, email):
+        usuario = Usuario.query.filter_by(email=email.data).first()
+        if usuario:
+            return ValidationError("E-mail já cadastrado, faça login para continuar")
 
 
-class FormLogin():
-    pass
 
-
-
-class FormCriarConta():
-    pass
 
 
--- ProjetoFakePinterest/main.py
+++ ProjetoFakePinterest/main.py
@@ -1,5 +1,8 @@
 # Rodar no terminal do projeto: pip install flask
 # Para utilizar login com criptografia rodar no terminal do projeto: pip install flask-login flask-bcrypt
+# Para criar formulário: pip install flask-wtf
+# Para validar e-mails: pip install email_validator
+
 from fakepinterest import app
 
 if __name__ == "__main__":
